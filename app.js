36dfc0ab52412a7731ad1246bfa4483bb3ca7597
dhanush7k <86766441+dhanush7k@users.noreply.github.com>
2024-09-01 20:54:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,36 @@ const body = document.getElementById("about")
 let content = document.querySelector("#hider")
 
 bar.addEventListener("click",()=>{
+    ulel.classList.toggle("show")const bar = document.getElementById("bar")
+const ulel = document.getElementById("ul")
+const body = document.getElementById("about")
+const fullBody = document.querySelector("body")
+let content = document.querySelector(".hider")
+
+bar.addEventListener("click",()=>{
     ulel.classList.toggle("show")
+    // fullBody.style.left=-100;
+    content.classList.toggle("move")
+
+
+})
+
+ulel.addEventListener("click",()=>{
+    ulel.classList.remove("show")
+})
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
     content.classList.toggle("move")
 
 })
